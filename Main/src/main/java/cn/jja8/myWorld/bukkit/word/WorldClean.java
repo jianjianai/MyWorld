e74cec7bb29f19a812a16ded2cca1244b12370d6
四叶草 <2801819967@qq.com>
2022-02-18 02:15:29 +0800

    微小的修改，使Forge+Bukkit服务端可以正常加载世界。

---
+++ Main/src/main/java/cn/jja8/myWorld/bukkit/word/WorldClean.java
@@ -0,0 +1,106 @@
+package cn.jja8.myWorld.bukkit.word;
+
+import cn.jja8.myWorld.bukkit.MyWorldBukkit;
+import org.bukkit.Location;
+import org.bukkit.World;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerTeleportEvent;
+import org.bukkit.scheduler.BukkitRunnable;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 用于卸载不需要被继续加载的世界
+ * */
+public class WorldClean  implements Listener {
+    List<PlayerWorlds> 空世界 = new ArrayList<>();
+    List<PlayerWorlds> 过期空世界 = new ArrayList<>();
+    List<PlayerWorlds> cleaningWorlds = new ArrayList<>();
+    public WorldClean(){
+        MyWorldBukkit.getMyWorldBukkit().getServer().getPluginManager().registerEvents(this, MyWorldBukkit.getMyWorldBukkit());
+        //扫描过期世界
+        new BukkitRunnable(){
+            @Override
+            public void run() {
+                cleaningWorlds.addAll(过期空世界);
+                过期空世界 = 空世界;
+                空世界 = new ArrayList<>();
+                //扫描空世界
+                List<PlayerWorlds> playerWorldsList = new ArrayList<>(MyWorldBukkit.getPlayerWordMangaer().nameMap.values());
+                playerWorldsList.removeAll(空世界);
+                playerWorldsList.removeAll(过期空世界);
+                playerWorldsList.removeAll(cleaningWorlds);
+                playerWorldsList.forEach(playerWord -> {
+                    if (playerWord.getWorld()!=null){
+                        if (playerWord.getWorld().getPlayers().size()>0){
+                            return;
+                        }
+                    }
+                    if (playerWord.getInfernalWorld()!=null){
+                        if (playerWord.getInfernalWorld().getPlayers().size()>0){
+                            return;
+                        }
+                    }
+                    if (playerWord.getEndWorld()!=null){
+                        if (playerWord.getEndWorld().getPlayers().size()>0){
+                            return;
+                        }
+                    }
+                    空世界.add(playerWord);
+                });
+            }
+        }.runTaskTimer(MyWorldBukkit.getMyWorldBukkit(), MyWorldBukkit.getWorldConfig().无玩家世界最短卸载时间*20, MyWorldBukkit.getWorldConfig().无玩家世界最短卸载时间*20);
+        //卸载过期世界
+        new BukkitRunnable(){
+            @Override
+            public void run() {
+                if (cleaningWorlds.size()>0){
+                    PlayerWorlds playerWorlds = cleaningWorlds.remove(0);
+                    try {
+                        MyWorldBukkit.getPlayerWordMangaer().unloadPlayerWorlds(playerWorlds,true);
+                    }catch (PlayerWordMangaer.worldBusy worldBusy){
+                        cleaningWorlds.add(playerWorlds);
+                    }
+                }
+            }
+        }.runTaskTimer(MyWorldBukkit.getMyWorldBukkit(), MyWorldBukkit.getWorldConfig().卸载空世界间隔时间*20, MyWorldBukkit.getWorldConfig().卸载空世界间隔时间*20);
+    }
+
+    @EventHandler
+    public void 玩家传送(PlayerTeleportEvent event){
+        Location lo = event.getTo();
+        if (lo==null){
+            return;
+        }
+        World word = lo.getWorld();
+        if (word==null){
+            return;
+        }
+        PlayerWorlds wo = MyWorldBukkit.getPlayerWordMangaer().getBeLoadPlayerWorlds(word);
+        if (wo==null){
+            return;
+        }
+        空世界.remove(wo);
+        过期空世界.remove(wo);
+        cleaningWorlds.remove(wo);
+    }
+
+    @EventHandler
+    public void 玩家进入(PlayerJoinEvent event){
+        World word = event.getPlayer().getLocation().getWorld();
+        if (word==null){
+            return;
+        }
+        PlayerWorlds wo = MyWorldBukkit.getPlayerWordMangaer().getBeLoadPlayerWorlds(word);
+        if (wo==null){
+            return;
+        }
+        空世界.remove(wo);
+        过期空世界.remove(wo);
+        cleaningWorlds.remove(wo);
+    }
+
+}
