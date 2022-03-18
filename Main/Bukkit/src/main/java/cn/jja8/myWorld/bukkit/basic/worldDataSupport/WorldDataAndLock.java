9b4ad2465711479e2d4ba8c1fe24f024d1e560b0
四叶草 <2801819967@qq.com>
2022-03-18 22:52:44 +0800

    完成了世界加载的实现

--- Main/Bukkit/src/main/java/cn/jja8/myWorld/bukkit/basic/worldDataSupport/WorldDataAndLock.java
+++ Main/Bukkit/src/main/java/cn/jja8/myWorld/bukkit/basic/worldDataSupport/WorldDataAndLock.java
@@ -2,33 +2,53 @@ package cn.jja8.myWorld.bukkit.basic.worldDataSupport;
 
 import cn.jja8.myWorld.all.veryUtil.FileLock;
 
-import java.io.File;
+import java.io.*;
 
 public abstract class WorldDataAndLock implements WorldDataLock{
-    File worldFile;//世界的数据文件夹
+    File worldDataFile;//世界的数据文件夹
     FileLock fileLock;
 
 
     @Override
-    public void unloadWorldAndUnlock(boolean save) {
-        unLoadWorld(save);
-        fileLock.unLock();
-    }
-
-    @Override
     public byte[] getCustomDataByte(String dataName) {
-
+        File file = new File(getWorldDataFile(),dataName+".data");
+        if (!file.exists()){
+            return null;
+        }
+        try(
+                FileInputStream fileInputStream = new FileInputStream(file);
+                ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        ){
+            int i; while ((i=fileInputStream.read())!=-1)byteArrayOutputStream.write(i);
+            return byteArrayOutputStream.toByteArray();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 
     @Override
     public void setCustomDataByte(String dataName, byte[] bytes) {
-        File file = new File(worldFile,dataName);
-
+        File file = new File(getWorldDataFile(),dataName+".data");
+        try {
+            file.createNewFile();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try(FileOutputStream fileOutputStream = new FileOutputStream(file)){
+            fileOutputStream.write(bytes);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
-    public File getWorldFile() {
-        return worldFile;
+    @Override
+    public void unlock() {
+        fileLock.unLock();
     }
 
-    abstract void unLoadWorld(boolean save);
+    public File getWorldDataFile() {
+        return worldDataFile;
+    }
+
 }
