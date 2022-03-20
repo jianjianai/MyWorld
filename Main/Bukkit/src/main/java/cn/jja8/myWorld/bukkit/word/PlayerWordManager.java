0019f7c4e157ed94694266b21fcd8203a47ef92c
四叶草 <2801819967@qq.com>
2022-03-20 17:05:14 +0800

    完成了世界加载

--- Main/Bukkit/src/main/java/cn/jja8/myWorld/bukkit/word/PlayerWordManager.java
+++ Main/Bukkit/src/main/java/cn/jja8/myWorld/bukkit/word/PlayerWordManager.java
@@ -1,11 +1,15 @@
 package cn.jja8.myWorld.bukkit.word;
 
+import cn.jja8.myWorld.all.basic.DatasheetSupport.Worlds;
 import cn.jja8.myWorld.bukkit.ConfigBukkit;
 import cn.jja8.myWorld.bukkit.MyWorldBukkit;
+import cn.jja8.myWorld.bukkit.basic.Teams;
 import cn.jja8.myWorld.bukkit.basic.WorldData;
+import cn.jja8.myWorld.bukkit.basic.worldDataSupport.WorldDataLock;
 import cn.jja8.myWorld.bukkit.config.Lang;
 import cn.jja8.myWorld.bukkit.config.WorldConfig;
 import cn.jja8.patronSaint_2022_3_2_1244.allUsed.file.YamlConfig;
+import com.esotericsoftware.yamlbeans.YamlException;
 import net.md_5.bungee.api.ChatMessageType;
 import net.md_5.bungee.api.chat.TextComponent;
 import org.bukkit.Bukkit;
@@ -22,6 +26,7 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.nio.charset.StandardCharsets;
 import java.util.*;
+import java.util.function.BiConsumer;
 import java.util.function.Consumer;
 
 /**
@@ -29,13 +34,10 @@ import java.util.function.Consumer;
  */
 public class PlayerWordManager implements Listener {
 
-    Lang lang = ConfigBukkit.getLang();
     WorldConfig worldConfig =  ConfigBukkit.getWorldConfig();
 
     Map<World, PlayerWorlds> wordMap = new HashMap<>();
-    Map<String, PlayerWorlds> nameMap = new HashMap<>();
-    Map<String, PlayerWorlds> loadingMap = new HashMap<>();
-    Map<PlayerWorlds, List<Consumer<PlayerWorlds>>> loadedMap = new HashMap<>();
+    Map<Worlds, PlayerWorlds> worldsMap = new HashMap<>();
 
     public PlayerWordManager() {
         MyWorldBukkit.getMyWorldBukkit().getServer().getPluginManager().registerEvents(this,MyWorldBukkit.getMyWorldBukkit());
@@ -43,13 +45,133 @@ public class PlayerWordManager implements Listener {
 
 
     /**
-     * 通过世界名称加载一个玩家世界
-     * @param name 世界唯一名称。
-     * @param consumer 世界加载完成后调用， null 如果世界被其他服务器加载
+     * 加载世界组,最好在异步调用，此方法有可能阻塞线程。
+     * @return null 有世界被其他服务器上锁，无法加载。
      */
-    public void loadPlayerWorlds(String name,Consumer<PlayerWorlds> consumer){
-        重新写
-     }
+    public PlayerWorlds loadPlayerWorlds(Worlds worlds){
+        synchronized (this){
+            PlayerWorlds po = worldsMap.get(worlds);
+            if (po!=null){
+                return po;
+            }
+
+            String worldsName = worlds.getWorldsName();
+            PlayerWordInform playerWordInform = new PlayerWordInform(worlds.getWorldsData("playerWordInform"));
+            PlayerWorlds playerWorlds = new PlayerWorlds(this,playerWordInform,worldsName,worlds);
+
+            List<String> worldList = worlds.getWorldList();
+
+            Map<WorldDataLock,String> worldDataLockNameMap = new HashMap<>();
+
+
+            //加载所有世界的锁
+            for (String s : worldList) {
+                WorldDataLock worldDataLock = WorldData.worldDataSupport.getWorldDataLock(s,worldConfig.服务器名称);
+                if (worldDataLock!=null) {
+                    worldDataLockNameMap.put(worldDataLock,s);
+                }
+            }
+            //判断世界全部上锁
+            if (worldDataLockNameMap.size()<worldList.size()){
+                for (WorldDataLock worldDataLock : worldDataLockNameMap.keySet()) {
+                    worldDataLock.unlock();
+                }
+                return null;
+            }
+
+            //获取全部世界的类型
+            Map<WorldDataLock,String> worldTypeMap = new HashMap<>();//最后用于加载到PlayerWorlds的map
+
+            for (WorldDataLock worldDataLock : worldDataLockNameMap.keySet()) {
+                byte[] bytes = worldDataLock.getCustomDataByte("WorldType");
+                if (bytes==null){
+                    worldTypeMap.put(worldDataLock,PlayerWorldTypeAtName.unknown.toString());
+                }
+                worldTypeMap.put(worldDataLock,new String(bytes,StandardCharsets.UTF_8));
+            }
+
+            //判断主世界，地狱，末地是否存在，如果不存在就根据配置文件添加
+            if (worldConfig.主世界生成器.启用){
+                if (!worldTypeMap.containsValue(PlayerWorldTypeAtName.world.toString())){
+                    String worldname = worldsName+"_"+PlayerWorldTypeAtName.world;
+                    WorldDataLock worldDataLock = WorldData.worldDataSupport.getWorldDataLock(worldname,worldConfig.服务器名称);
+                    if (worldDataLock!=null){
+                        worldTypeMap.put(worldDataLock,PlayerWorldTypeAtName.world.toString());
+                        worldDataLockNameMap.put(worldDataLock,worldname);
+                        worlds.putWorld(worldname);
+                    }else {
+                        MyWorldBukkit.getMyWorldBukkit().getLogger().warning("新的世界"+worldname+"无法获得锁。可能是已经被其他服务器加载，或世界命名规则错乱导致。"+worldsName+"世界组将会没有主世界。");
+                    }
+                }
+            }
+            if (worldConfig.地狱界生成器.启用){
+                if (!worldTypeMap.containsValue(PlayerWorldTypeAtName.infernal.toString())){
+                    String worldname = worldsName+"_"+PlayerWorldTypeAtName.infernal;
+                    WorldDataLock worldDataLock = WorldData.worldDataSupport.getWorldDataLock(worldname,worldConfig.服务器名称);
+                    if (worldDataLock!=null){
+                        worldTypeMap.put(worldDataLock,PlayerWorldTypeAtName.infernal.toString());
+                        worldDataLockNameMap.put(worldDataLock,worldname);
+                        worlds.putWorld(worldname);
+                    }else {
+                        MyWorldBukkit.getMyWorldBukkit().getLogger().warning("新的世界"+worldname+"无法获得锁。可能是已经被其他服务器加载，或世界命名规则错乱导致。"+worldsName+"世界组将会没有地狱世界。");
+                    }
+                }
+            }
+            if (worldConfig.末地界生成器.启用){
+                if (!worldTypeMap.containsValue(PlayerWorldTypeAtName.end.toString())){
+                    String worldname = worldsName+"_"+PlayerWorldTypeAtName.end;
+                    WorldDataLock worldDataLock = WorldData.worldDataSupport.getWorldDataLock(worldname,worldConfig.服务器名称);
+                    if (worldDataLock!=null){
+                        worldTypeMap.put(worldDataLock,PlayerWorldTypeAtName.end.toString());
+                        worldDataLockNameMap.put(worldDataLock,worldname);
+                        worlds.putWorld(worldname);
+                    }else {
+                        MyWorldBukkit.getMyWorldBukkit().getLogger().warning("新的世界"+worldname+"无法获得锁。可能是已经被其他服务器加载，或世界命名规则错乱导致。"+worldsName+"世界组将会没有末地世界。");
+                    }
+                }
+            }
+
+            //将全部世界加载到PlayerWorlds
+            worldTypeMap.forEach((worldDataLock, s) -> {
+                byte[] bytes = worldDataLock.getCustomDataByte("WorldCreator");
+                WorldConfig.WorldBuilder worldBuilder = null;
+                if (bytes==null){
+                    PlayerWorldTypeAtName playerWorldTypeAtName;
+                    try {
+                        playerWorldTypeAtName = PlayerWorldTypeAtName.valueOf(s);
+                    }catch (IllegalArgumentException illegalArgumentException){
+                        playerWorldTypeAtName = null;
+                    }
+                    if (playerWorldTypeAtName==null){
+                        worldBuilder = worldConfig.主世界生成器;
+                    }else {
+                        switch (playerWorldTypeAtName){
+                            case world:
+                            case unknown:worldBuilder = worldConfig.主世界生成器;break;
+                            case infernal:worldBuilder = worldConfig.地狱界生成器;break;
+                            case end:worldBuilder = worldConfig.末地界生成器;break;
+                        }
+                    }
+                    try {
+                        worldDataLock.setCustomDataByte("WorldCreator",YamlConfig.saveToString(worldBuilder).getBytes(StandardCharsets.UTF_8));
+                    } catch (YamlException e) {
+                        e.printStackTrace();
+                    }
+                }
+                if (worldBuilder==null){
+                    try {
+                        worldBuilder = YamlConfig.loadFromString(new String(bytes,StandardCharsets.UTF_8),WorldConfig.WorldBuilder.class);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                        worldBuilder = worldConfig.主世界生成器;
+                    }
+                }
+                playerWorlds.putWorld(s,worldDataLock,worldBuilder.getWordBuilder(worldDataLockNameMap.get(worldDataLock)));
+            });
+            worldsMap.put(worlds,playerWorlds);
+            return playerWorlds;
+        }
+    }
 
     /**
      * 从已加载的世界中获取世界
@@ -63,42 +185,28 @@ public class PlayerWordManager implements Listener {
      * 从已加载的世界中获取世界
      * @return 如果世界没加载返回null
      */
-    public PlayerWorlds getBeLoadPlayerWorlds(String PlayerWorldName){
-        return nameMap.get(PlayerWorldName);
+    public PlayerWorlds getBeLoadPlayerWorlds(Worlds worlds){
+        return worldsMap.get(worlds);
     }
 
     /**
-     * 被加载的世界名称集合
+     * 被加载的世界称集合
      * */
-    public Set<String> getWorldNames(){
-        return nameMap.keySet();
+    public Set<Worlds> getWorldNames(){
+        return worldsMap.keySet();
     }
     /**
      * 获取被加载的世界集合
      * */
     public Collection<PlayerWorlds> getWorlds(){
-        return nameMap.values();
+        return worldsMap.values();
     }
-
-    /**
-     * 卸载一个玩家世界
-     */
-     public void unloadPlayerWorlds(PlayerWorlds playerWord, boolean save) {
-         重新写
-     }
 
     /**
      * 判断这个世界是否存在
      */
     public boolean isWorldExistence(String name){
         return WorldData.worldDataSupport.isWorldExistence(name);
-    }
-    /**
-     * 获取某个世界的信息.
-     * @return 如果世界不存在就返回null
-     */
-    public PlayerWordInform getPlayerWordInform(String name){
-        重新写
     }
     /**
      * 关闭世界管理器
@@ -112,7 +220,7 @@ public class PlayerWordManager implements Listener {
     /**
      * 删除世界
      */
-    public void delPlayerWorlds(String worldName) {
+    public void delPlayerWorlds(Worlds worlds) {
         重新写
     }
 
--- Main/Bukkit/src/main/java/cn/jja8/myWorld/bukkit/word/PlayerWorldTypeAtName.java
+++ Main/Bukkit/src/main/java/cn/jja8/myWorld/bukkit/word/PlayerWorldTypeAtName.java
@@ -3,5 +3,6 @@ package cn.jja8.myWorld.bukkit.word;
 public enum PlayerWorldTypeAtName {
     world,//主世界
     infernal,//地狱
-    end//末地
+    end,//末地
+    unknown,//未知
 }
